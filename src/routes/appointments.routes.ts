b0f369c0f2b167973dd6022458605d625b62a5e5
Newton Duarte <newton_@hotmail.com>
2020-09-02 06:48:30 -0300

    First nodejs with typescript project from gostack bootcamp

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+import { parseISO } from 'date-fns';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+  
+    const parsedDate = parseISO(date);
+    
+    const createAppointment = new CreateAppointmentService(appointmentsRepository);
+  
+    const appointment = createAppointment.execute({ provider, date: parsedDate });
+  
+    return response.status(201).json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
