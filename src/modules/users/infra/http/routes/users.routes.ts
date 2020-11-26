093b5b0e45c7b12c214ab8d08803425c9fa873b6
Newton Duarte <newton_@hotmail.com>
2020-11-26 07:15:28 -0300

    Added routes validation with celebrate and Joi

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import multer from 'multer';
+
 import uploadConfig from '@config/upload';
 
 import UsersController from '../controllers/UsersController';
@@ -13,7 +15,17 @@ const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    }
+  }),
+  usersController.create
+);
 
 usersRouter.patch(
   '/avatar',
