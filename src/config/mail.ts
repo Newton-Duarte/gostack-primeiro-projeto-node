0e676c158974f3ce06817976508766c2d1e3540a
Newton Duarte <newton_@hotmail.com>
2020-12-02 07:41:31 -0300

    Added SESMailProvider and AWS-SDK config to send emails

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'newton_@hotmail.com',
+      name: 'Newton Duarte Cavalcanti'
+    }
+  }
+} as IMailConfig;
