4472c456a81f92283473b95751fb1eb862bc436c
Newton Duarte <newton_@hotmail.com>
2020-12-04 08:36:33 -0300

    Added Cache to ListProviderAppointmentsService

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -8,12 +8,10 @@ export default class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const { provider_id, date } = request.body;
-  
-    const parsedDate = parseISO(date);
 
     const createAppointment = container.resolve(CreateAppointmentService);
 
-    const appointment = await createAppointment.execute({ provider_id, user_id, date: parsedDate });
+    const appointment = await createAppointment.execute({ provider_id, user_id, date });
 
     return response.status(201).json(appointment);
   }
