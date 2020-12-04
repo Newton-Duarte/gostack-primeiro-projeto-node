a81521c9b00ea533f432ca6d8ab2b843e5746808
Newton Duarte <newton_@hotmail.com>
2020-12-04 08:54:34 -0300

    Added FakeCacheProvider and refactor tests

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,29 +2,33 @@ import AppError from '@shared/errors/AppError';
 
 import FakeNotificationsRepository from '@modules/notifications/repositories/fakes/FakeNotificationsRepository';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 import CreateAppointmentService from './CreateAppointmentService';
 
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
 let fakeNotificationsRepository: FakeNotificationsRepository;
+let fakeCacheProvider: FakeCacheProvider;
 let createAppointment: CreateAppointmentService;
 
 describe('CreateAppointment', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
     fakeNotificationsRepository = new FakeNotificationsRepository();
+    fakeCacheProvider = new FakeCacheProvider();
     createAppointment = new CreateAppointmentService(
       fakeAppointmentsRepository,
-      fakeNotificationsRepository
+      fakeNotificationsRepository,
+      fakeCacheProvider
     );
   });
 
   it('should be able to create a new appointment', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date(2020, 10, 25, 12).getTime();
+      return new Date(2020, 11, 25, 12).getTime();
     });
 
     const appointment = await createAppointment.execute({
-      date: new Date(2020, 10, 25, 13),
+      date: new Date(2020, 11, 25, 13),
       user_id: 'user-id',
       provider_id: 'provider-id'
     });
@@ -34,7 +38,7 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same date', async () => {
-    const appointmentDate = new Date(2020, 10, 25, 8);
+    const appointmentDate = new Date(2020, 11, 25, 8);
 
     await createAppointment.execute({
       date: appointmentDate,
@@ -67,12 +71,12 @@ describe('CreateAppointment', () => {
 
   it('should not be able to create an appointment with same user as provider', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date(2020, 10, 25, 12).getTime();
+      return new Date(2020, 11, 25, 12).getTime();
     });
 
     await expect(
       createAppointment.execute({
-        date: new Date(2020, 10, 25, 11),
+        date: new Date(2020, 11, 25, 11),
         user_id: 'user-id',
         provider_id: 'user-id'
       })
@@ -86,7 +90,7 @@ describe('CreateAppointment', () => {
 
     await expect(
       createAppointment.execute({
-        date: new Date(2020, 10, 25, 7),
+        date: new Date(2020, 11, 25, 7),
         user_id: 'user-id',
         provider_id: 'provider-id'
       })
@@ -94,7 +98,7 @@ describe('CreateAppointment', () => {
 
     await expect(
       createAppointment.execute({
-        date: new Date(2020, 10, 25, 18),
+        date: new Date(2020, 11, 25, 18),
         user_id: 'user-id',
         provider_id: 'provider-id'
       })
--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,13 +1,27 @@
 import AppError from '@shared/errors/AppError';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 import CreateUserService from './CreateUserService';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let fakeCacheProvider: FakeCacheProvider;
+let createUser: CreateUserService;
+
 describe('CreateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    fakeCacheProvider = new FakeCacheProvider();
+    createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+      fakeCacheProvider
+    );
+  });
+
   it('should be able to create a new user', async () => {
-    const fakeHashProvider = new FakeHashProvider();
-    const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -19,13 +33,9 @@ describe('CreateUser', () => {
   });
 
   it('should not be able to create a new user with same email from another', async () => {
-    const fakeHashProvider = new FakeHashProvider();
-    const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
-
     const userEmail = 'johndoe@example.com';
 
-    const user = await createUser.execute({
+    await createUser.execute({
       name: 'John Doe',
       email: userEmail,
       password: '123456'
