be8284a6e69137ccec61d8dab822c86226e13b0b
Newton Duarte <newton_@hotmail.com>
2020-12-05 09:13:49 -0300

    Fix AppointmentsRepository

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -50,7 +50,8 @@ class AppointmentsRepository implements IAppointmentsRepository {
           dateFieldName =>
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`
         )
-      }
+      },
+      relations: ['user']
     });
     
     return appointments;
